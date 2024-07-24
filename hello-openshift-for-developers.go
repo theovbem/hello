a6fff7d25ae9ea5fc8e2e432630295de014eda8e
theovbem <theovbem@gmail.com>
2024-07-24 21:50:03 +0200

    Updated hello message

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,11 +9,11 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello The0penShift!"
 	}
 
 	fmt.Fprintln(w, response)
-	fmt.Println("Servicing an impatient beginner's request.")
+	fmt.Println("Servicing an experienced beginner's request.")
 }
 
 func listenAndServe(port string) {
